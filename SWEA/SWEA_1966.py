f76f800b91a3936f38d130ba515e2f21455da777
이은석 <modelism93@naver.com>
2023-02-02 23:52:57 +0900

    SWEA 1966번 주석 수정

--- SWEA/SWEA_1966.py
+++ SWEA/SWEA_1966.py
@@ -8,14 +8,14 @@ sys.stdin = open("input.txt", "r")
 
 T = int(input())
 for tc in range(1, T+1):
-    N = int(input())
-    if N < 5 or N > 50:    # 범위 초과 예외 처리
-        print('N 범위를 초과했습니다.')
+    N = int(input())    # N개의 숫자
 
-    lst = list(map(int, input().split()))
+    lst = list(map(int, input().split()))  
+    # print(lst)    # [15, 20, 8, 28, 16, 27, 17, 27, 10, 12]
 
-    # counts 의 사이즈 설정
-    size = 1
+    # counts : lst에 있는 숫자를 인덱스로 받아 몇개 들어있는지 확인하는 리스트
+    # counts는 lst에 있는 숫자의 최대값으로 사이즈를 설정한다.
+    size = 1   # 최소한 1개의 숫자는 있을 것이기에 size 변수로 설정
     for x in range(N):
         if (lst[x]) > size:
             size = (lst[x])
@@ -26,21 +26,21 @@ for tc in range(1, T+1):
     for x in lst:
         counts[x] += 1          # 각 숫자에 대한 counts의 idx자리에 카운트가 +1 씩 됨
     # print(counts)     #[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 2, 1]
-    for x in range(1, size + 1):
-        counts[x] += counts[x-1]     # counts 리스트 변경, 앞에 것을 더해서 각 자리를 찾아갈 것
+    for i in range(1, size + 1):
+        counts[i] += counts[i-1]     # counts 누적합 리스트를 만듦, lst안에 있는 숫자가 몇번째로 들어갈지 알 수 있게 됨
     # print(counts)     #[0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 3, 4, 5, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 9, 10]
 
-    temp = [0]*N    # 각 숫자를 오름차순으로 정렬해줄 temp 설정
+    temp_lst = [0]*N    # lst와 사이즈가 같은 임시 리스트
 
     for i in range(N-1, -1, -1):    # lst를 역순으로 확인
         counts[lst[i]] -= 1
         # lst의 숫자가 counts의 idx로 표현되어있으니 count의 그 idx에 있는 숫자가
         # temp에서 정렬되었을때 몇번째 자리인지 나타내는 것
-        temp[counts[lst[i]]] = lst[i]    #temp의 위치에 lst 숫자를 넣음
+        temp_lst[counts[lst[i]]] = lst[i]    #temp의 위치에 lst 숫자를 넣음
 
-    print(temp)    #[8, 10, 12, 15, 16, 17, 20, 27, 27, 28]
+    print(temp_lst)    #[8, 10, 12, 15, 16, 17, 20, 27, 27, 28]
 
-    result = map(str, temp)       # 각 숫자를 str로 바꾸어줌.
+    result = map(str, temp_lst)       # 각 숫자를 str로 바꾸어줌.
     res = ' '.join(result)
     print(f'#{tc} {res}')
 
