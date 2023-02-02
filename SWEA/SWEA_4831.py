2027a93b9fdcbac5e0ade89985c1174c141431cb
이은석 <modelism93@naver.com>
2023-02-03 00:05:56 +0900

    SWEA 4831번번 주석 수정

--- SWEA/SWEA_4831.py
+++ SWEA/SWEA_4831.py
@@ -1,33 +1,46 @@
 # SWEA 4831번 전기버스
+'''
+전기버스가 최소한 몇번의 충전소를 거쳐서 끝까지 갈 수 있는지 확인하는 문제
+
+3 10 5
+1 3 5 7 9
+
+'''
+
+
 import sys
 sys.stdin = open("input.txt", "r")
 
 T = int(input())
 for tc in range(1, T+1):
-    K,N,M = map(int, input().split())
-    M_list = list(map(int, input().split()))     # 입력값을 받음
+    K,N,M = map(int, input().split())    
+    # K = 한번 충전으로 이동하는 정류장 수
+    # N = 전체 정류장 수
+    # M = 충전기가 설치된 정류장 번호
 
-    N_list = list(range(N+1))    # 전체 정류장의 list를 만들어줌
+    charge_station = list(map(int, input().split()))     # 충전기가 있는 정류소 리스트
 
-    max_D = K    # 최대 갈 수 있는 거리를 K값으로 1차적으로 고정해줌
-    i = 0
-    oil_cnt = 0
+    station = list(range(N+1))    # 전체 정류장의 리스트
+
+    max_D = K       # 한번의 충전으로 최대 갈 수 있는 거리를 K값으로 1차적으로 고정해줌
+    i = 0           # 버스가 출발하는 기준점
+    charge_cnt = 0  # 충전 횟 수 
 
     while True:
 
         if i >= N - max_D:    # 마지막 충전기로부터 끝까지 갈 수 있으면 충전기가 이제 필요 없으므로 break
             break
-
-        if N_list[i+max_D] in M_list:    # 충전한 것으로 갈 수 있는 최대 거리에 충전기가 있으면
-            oil_cnt += 1        # oil cnt를 하나 추가
+        # elif부분을 먼저 설명하고 if 문을 설명하는게 매끄러움
+        if station[i+max_D] in charge_station:    # 충전한 것으로 갈 수 있는 최대 거리에 충전기가 있으면
+            charge_cnt += 1        # oil cnt를 하나 추가
             i = i + max_D       # max_D를 더한 위치까지 보냄
             max_D = K           # max_D값 초기화
-            continue
+            # continue
 
-        elif N_list[i+max_D] not in M_list:    # 최대치로 갈수 있는 장소에 충전기가 없으면
+        elif station[i+max_D] not in charge_station:    # 최대치로 갈수 있는 장소에 충전기가 없으면
             max_D = max_D-1     # 1만큼 더 가까운 곳에 있는지 확인하기 위해 max_D를 줄임
             if max_D == 0:      # max_D가 0이면 최대로 가는 곳에 충전기가 없으므로
-                oil_cnt = 0     # 끝까지 못가므로 oil cnt를 0으로 넣고 마무리
+                charge_cnt = 0     # 끝까지 못가므로 oil cnt를 0으로 넣고 마무리
                 break
 
-    print(f'#{tc} {oil_cnt}')
+    print(f'#{tc} {charge_cnt}')
