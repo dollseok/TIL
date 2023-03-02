0ea3d06ac769d3fc8d061e1e775c2005444728e3
은석 <122436585+euneuneunseok@users.noreply.github.com>
2023-03-02 20:33:09 +0900

    [D3] Title: 최대 성적표 만들기, Time: 190 ms, Memory: 61,484 KB -BaekjoonHub

--- SWEA/D3/4466. 최대 성적표 만들기/최대 성적표 만들기.py
+++ SWEA/D3/4466. 최대 성적표 만들기/최대 성적표 만들기.py
@@ -3,10 +3,10 @@ for tc in range(1,T+1):
     N,K = map(int,input().split())
     scores = list(map(int,input().split()))
 
-    scores.sort()
+    scores.sort(reverse=True)
 
     score_sum = 0
     for i in range(K):
-        score_sum += scores[N-i-1]
+        score_sum += scores[i]
 
     print(f'#{tc}', score_sum)
