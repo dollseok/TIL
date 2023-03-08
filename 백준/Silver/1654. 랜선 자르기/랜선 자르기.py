aa54e59f4e2ae9de7d14d74bf65212b54baa9b9b
이은석 <modelism93@naver.com>
2023-03-08 17:55:19 +0900

    백준 class2 1654 랜선자르기 주석 추가

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -10,6 +10,17 @@
 K의 최소값부터 하나씩 생각
 '''
 
+'''
+내가 계속 틀린 이유
+mx = max(lst) + 1
+이 부분에서 마지막 값도 봐야하는데 +1을 안해주면 마지막 것을 확인을 못해보는 것
+이진 탐색으로 풀어야함
+
+'''
+
+# import sys
+# sys.stdin = open('input.txt', 'r')
+
 K,N = map(int, input().split())
 lst = [int(input()) for _ in range(K)]
 
@@ -21,23 +32,23 @@ mn = 1
 mx = max(lst) + 1
 
 while True:
-    middle = (mn + mx) // 2
+    middle = (mn + mx) // 2     # 가장 큰값+1 과 1의 중간을 탐색
     for i in lst:
-        sum_ += i//middle
+        sum_ += i//middle       # 몫의 합들을 구함
 
-    if sum_ >= N:
-        if (sum_,mn,mx) in res_list:
-            res = mn
+    if sum_ >= N:                      # 자른 랜선의 개수가 N보다 크면
+        if (sum_,mn,mx) in res_list:   # 튜플로 만든 값이 이미 결과에 있으면
+            res = mn                   # 아래에서 부터 올라오면서 찾았으므로 결과는 mn
             break
-        res_list.append((sum_,mn,mx))
+        res_list.append((sum_,mn,mx))  # 합, 최소값, 최대값을 튜플로 결과 리스트에 넣음
         mn = middle
         sum_ = 0
 
-    else:
-        if (sum_,mn,mx) in res_list:
-            res = mx
+    else:                              # 자른 랜선의 개수가 N보다 작으면
+        if (sum_,mn,mx) in res_list:   # 튜플로 만든 값이 이미 결과에 있으면
+            res = mx                   # 위에서부터 내려오면서 찾았으므로 결과는 mx
             break
-        mx = middle
+        mx = middle                    # N보다 작은건 결과가 아님으로 결과에 추가하진 않음
         sum_ = 0
 
 
