bb91c1c4e9f84c9e1d2b13e962f98037594d63af
은석 <122436585+euneuneunseok@users.noreply.github.com>
2023-03-09 21:54:47 +0900

    [Silver III] Title: 소수 구하기, Time: 268 ms, Memory: 38928 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -8,34 +8,34 @@ M 이상 N이하의 소수를 모두 출력하는 프로그램
 # import sys
 # sys.stdin = open('input.txt', 'r')
 
-M, N = map(int,input().split())
-res = [1] * (N+1)
-
-for i in range(2, N+1):
-    for j in range(2, N+1):
-        if i*j > N:
-            break
-        res[i*j] = 0
-
-for i in range(M,N+1):
-    if res[i] and i != 1:
-        print(i)
+# M, N = map(int,input().split())
+# res = [1] * (N+1)
+# 
+# for i in range(2, N+1):
+#     for j in range(2, N+1):
+#         if i*j > N:
+#             break
+#         res[i*j] = 0
+# 
+# for i in range(M,N+1):
+#     if res[i] and i != 1:
+#         print(i)
 
 
 # 에라토스테네스의 수
 
-# M, N = map(int, input().split())
-# res = [1]*(N+1)    # 인덱스 활용
-# K = int(N**0.5)    # 그보다 작은 제곱수까지의 소수까지만 확인
-#
-# for i in range(2,K+1):
-#     if res[i]:
-#         for j in range(i+i, N+1, i):    # 소수 i의 배수들 확인
-#             res[j] = 0
-#
-# for i in range(M, N+1):
-#     if res[i] and i != 1:
-#         print(i)
+M, N = map(int, input().split())
+res = [1]*(N+1)    # 인덱스 활용
+K = int(N**0.5)    # 그보다 작은 제곱수까지의 소수까지만 확인
+
+for i in range(2,K+1):
+    if res[i]:
+        for j in range(i+i, N+1, i):    # 소수 i의 배수들 확인
+            res[j] = 0
+
+for i in range(M, N+1):
+    if res[i] and i != 1:
+        print(i)
 
 
 
