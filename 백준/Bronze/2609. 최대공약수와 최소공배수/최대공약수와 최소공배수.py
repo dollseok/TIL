444c97dcd4553da017e5e59ec0ec038e5b98ccb4
은석 <122436585+euneuneunseok@users.noreply.github.com>
2023-03-17 17:39:12 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -18,25 +18,25 @@ else:
 
 
 
-if N != 1 and M != 1:
-    for i in range(startnum,0,-1):
-        if N % i == 0:
-            if M % i == 0:
-                res1 = i
-                break
-
-    print(res1)
-
-    for i in range(1,startnum+1):
-        if othernum*i % startnum == 0:
-            res2 = othernum*i
+# if N != 1 and M != 1:
+for i in range(startnum,0,-1):
+    if N % i == 0:
+        if M % i == 0:
+            res1 = i
             break
-    print(res2)
 
-elif N == 1:
-    print(1)
-    print(M)
+print(res1)
 
-elif M == 1:
-    print(1)
-    print(N)
+for i in range(1,startnum+1):
+    if othernum*i % startnum == 0:
+        res2 = othernum*i
+        break
+print(res2)
+
+# elif N == 1:
+#     print(1)
+#     print(M)
+#
+# elif M == 1:
+#     print(1)
+#     print(N)
