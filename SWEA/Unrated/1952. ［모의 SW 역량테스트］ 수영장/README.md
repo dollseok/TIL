b3f0c0135570c38c3c4dff1005b1da8a2c02e99a
은석 <122436585+euneuneunseok@users.noreply.github.com>
2023-03-31 13:42:49 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 363 ms, Memory: 63,832 KB -BaekjoonHub

---
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/README.md
@@ -0,0 +1,11 @@
+# [Unrated] [모의 SW 역량테스트] 수영장 - 1952 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PpFQaAQMDFAUq) 
+
+### 성능 요약
+
+메모리: 63,832 KB, 시간: 363 ms, 코드길이: 526 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.py
@@ -0,0 +1,27 @@
+# SWEA 1952번 모의 SW 역량테스트. 수영장
+'''
+
+'''
+
+def dfs(month, pay):
+    global min_pay
+    if month > 12:
+        if min_pay > pay:
+            min_pay = pay
+        return
+
+    dfs(month+1, pay + day*lst[month])
+    dfs(month+1, pay + mon)
+    dfs(month+3, pay + mon3)
+    dfs(month+12, pay+year)
+
+
+T = int(input())
+for test_case in range(1,T+1):
+    day, mon, mon3, year = map(int, input().split())
+    lst = [0] + list(map(int,input().split()))
+
+    min_pay = 3000*365
+    dfs(1,0)
+
+    print(f'#{test_case} {min_pay}')
