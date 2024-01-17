2441f1657554dfd749c1bffea52a41ee407ff3ff
은석 <122436585+dollseok@users.noreply.github.com>
2024-01-17 17:00:17 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,41 +1,17 @@
-'''
-1541번 잃어버린 괄호
 
-양수 , + , - , 괄호로 식을 만듦 but 다 지움
-괄호를 적절히 쳐서 식의 값을 최소로
-'''
+math_eq = input()
+start = math_eq[0]
+item_list = math_eq.split('-')
+result = 0
 
-math_eq = str(input())
+if start == '-':
+    print(item_list)
+    for item in item_list[1:]:
+        result -= sum(map(int, item.split('+')))
 
-num_list = ''
-num = ''
-bracket = False
+else:
+    result += sum(map(int, item_list[0].split('+')))
+    for item in item_list[1:]:
+        result -= sum(map(int, item.split('+')))
 
-
-for i in range(len(math_eq)):
-    e = math_eq[i]
-    if e == '-':
-        if not bracket:
-            num_list += num + e + '('
-            bracket = True
-            num = ''
-
-        else:
-            num_list += num + ')' + e + '('
-            num = ''
-
-    elif e == '+':
-        num_list += num + '+'
-        num = ''
-    else:
-        if num == '0':
-            num = e
-        else:
-            num += e
-
-    if i == len(math_eq) - 1:
-        num_list += num
-        if bracket:
-            num_list += ')'
-
-print(eval(num_list))
+print(result)
