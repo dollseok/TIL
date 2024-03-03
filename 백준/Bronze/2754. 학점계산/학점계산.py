7cd02c5c70011c9afe7790461fec5258dd07569e
은석 <122436585+dollseok@users.noreply.github.com>
2024-03-03 18:54:05 +0900

    [Bronze V] Title: 학점계산, Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,28 @@
+data = input()
+
+if data == 'A+':
+    print('4.3')
+elif data == 'A0':
+    print('4.0')
+elif data == 'A-':
+    print('3.7')
+elif data == 'B+':
+    print('3.3')
+elif data == 'B0':
+    print('3.0')
+elif data == 'B-':
+    print('2.7')
+elif data == 'C+':
+    print('2.3')
+elif data == 'C0':
+    print('2.0')
+elif data == 'C-':
+    print('1.7')
+elif data == 'D+':
+    print('1.3')
+elif data == 'D0':
+    print('1.0')
+elif data == 'D-':
+    print('0.7')
+else:
+    print('0.0')
