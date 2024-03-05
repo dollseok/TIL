32e8f7ff6850b4759a8dda11a57102bdfc328b81
은석 <122436585+dollseok@users.noreply.github.com>
2024-03-05 17:09:47 +0900

    [Gold III] Title: 택배, Time: 448 ms, Memory: 115468 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.py
+++ 백준/Gold/1719. 택배/택배.py
@@ -1,49 +1,48 @@
 '''
 1719번 택배
+오기직전 경로에 무엇이 있었는지 출력
 '''
+import heapq
 
-from heapq import heappop,heappush
-
-def dijkstra(start,n):
-    global dist_arr,res
-
+def dijkstra(start, end):
     hq = []
-    for i in range(1,n+1):
-        if i == start:
-            continue
-        heappush(hq, (dist_arr[start][i], i))
+    heapq.heappush(hq, (0,start))
 
     while hq:
-        dist, node = heappop(hq)
-        if dist_arr[start][node] < dist: # 거리가 길면 넘어감
-            continue
-        for neighbor in range(1,n+1):
-            if node == neighbor or start == neighbor:
+        dist, node = heapq.heappop(hq)
+        for next_dist, next_node in connect[node]:
+            if dist_arr[start][next_node] < dist + next_dist:
                 continue
-            if dist + dist_arr[node][neighbor] < dist_arr[start][neighbor]:
-                dist_arr[start][neighbor] = dist + dist_arr[node][neighbor]
-                heappush(hq,(dist+dist_arr[node][neighbor],neighbor))
+            else:
+                dist_arr[start][next_node] = dist + next_dist
+                heapq.heappush(hq,(dist + next_dist, next_node))
+                result[next_node][start] = node
 
-                res[start][neighbor] = res[start][node]
 
 
-n,m = map(int,input().split())
-INF = 1e9
-dist_arr = [[INF]*(n+1) for _ in range(n+1)]
-res = [[0]*(n+1) for _ in range(n+1)]
+v,e = map(int,input().split())
+INF = int(1e9)
 
-for i in range(n+1):
-    res[i][i] = '-'
+connect = [[] for _ in range(v+1)]
+dist_arr = [[INF]*(v+1) for _ in range(v+1)] # 현재 연결되어 있는 거리
+result = [[0]*(v+1) for _ in range(v+1)] # 어디로 가는 것이 가장 빠른 경로인지
 
-for _ in range(m):
-    a,b,dst = map(int,input().split())
-    dist_arr[a][b] = dst
-    dist_arr[b][a] = dst
-    res[a][b] = b
-    res[b][a] = a
+for i in range(v+1):
+    dist_arr[i][i] = 0
+    result[i][i] = '-'
 
-for i in range(1,n+1):
-    dijkstra(i,n)
+for _ in range(e):
+    s,e,d = map(int,input().split())
+    connect[s].append([d,e])
+    connect[e].append([d,s])
+    dist_arr[s][e] = d
+    dist_arr[e][s] = d
+    result[s][e] = e
+    result[e][s] = s
 
-for line in res[1:]:
-    print(*line[1:])
+# 출발지에서 그 지점으로 가는데 가장 빠른 경로 찾기
+for i in range(1,v+1):
+    dijkstra(i,v)
+
+for i in range(1,v+1):
+    print(*result[i][1:])
