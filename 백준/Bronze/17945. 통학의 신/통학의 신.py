1927afe716027df8f3a8b96397c973b24e493c19
은석 <122436585+dollseok@users.noreply.github.com>
2024-08-14 13:39:57 +0900

    [Bronze III] Title: 통학의 신, Time: 84 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/17945. 통학의 신/통학의 신.py
+++ 백준/Bronze/17945. 통학의 신/통학의 신.py
@@ -1,8 +1,24 @@
+'''
+x^2 + 2Ax + B = 0 조건을 만족하는 x 근 구하기
+시간 제한 1초 => 1억번 계산
+-1000 <= A,B <= 1000
+
+
+1. 근 두개의 곱이 -1000 ~ 1000이라는 것, 근 두개의 합이 -2000 ~ 2000이하라는 것
+=>x 의 범위를 -1000에서 1000정도면 다 나올 것 같음
+
+2. 근의 공식
+3. 근을 구하는 방식을 이용
+
+'''
+
 A,B = map(int,input().split())
 result = []
 
-for i in range(-2000,2001):
+for i in range(-1000,1001):
     if i*i + 2*A*i + B == 0:
         result.append(i)
+    if len(result) >= 2:
+        break
 
 print(*result)
