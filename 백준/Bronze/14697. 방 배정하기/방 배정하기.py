e24fee0c3804e30f4c3e233ca8483ddc5bf1960a
은석 <122436585+dollseok@users.noreply.github.com>
2024-08-18 19:27:12 +0900

    [Bronze II] Title: 방 배정하기, Time: 84 ms, Memory: 109240 KB, Score: 3 point -BaekjoonHub

---
+++ 백준/Bronze/14697. 방 배정하기/방 배정하기.py
@@ -0,0 +1,29 @@
+'''
+A B C N
+
+A,B,C 각자 인원숨만큼 방에 넣을 수 있음, 총인원수 N명
+
+빈침대 없이 방 배정 가능하면 1 불가능하면 0
+
+5 9 12 113 -> 1
+
+퓰이
+
+A,B,C 다해서 최대 50 * 50 * 50 시간 2초 충분할 듯
+
+'''
+
+
+def check_avail(A,B,C,N):
+
+    for i in range(1,51):
+        for j in range(i,51):
+            for k in range(j,51):
+                if A*i + B*j + C*k == N:
+                    return 1
+
+    return 0
+
+a,b,c,n = map(int,input().split())
+
+print(check_avail(a,b,c,n))
