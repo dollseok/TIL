acb8b2262677b4e21570c1e1bb5552c0d0124ea9
은석 <122436585+dollseok@users.noreply.github.com>
2024-08-21 13:47:39 +0900

    [Silver III] Title: 수열, Time: 108 ms, Memory: 122976 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,22 +1,41 @@
-# Baekjoon 2559번 수열
-
 '''
-측정한 온도가 어떤 정수의 수열로 주어졌다
-연속적인 며칠 동안 온도합이 가장 큰 값?
+수열
 
+연속적인 며칠동안 온도합이 가장 큰값
+
+연속적인 K 개의 합
+prefix 를 구하고 N
+
+prefix를 구간별로 한번 더 돌려서 k 구간 값을 구함 N
+
+=> 2N정도의 시간 복잡도를 가질 것
+
+반례
+
+-> 앞에 0인 부분이 최대값이 되는 경우를 제외해야한다.
+-> 리스트를 애초에 작게 만들던가, 아니면 K ->N 까지 리스트를 순회하며 최대값을 뽑는게 안정적
 '''
 
-# import sys
-# sys.stdin = open('input.txt', 'r')
+N,K = map(int,input().split())
 
-N, K = map(int, input().split())
-data = list(map(int, input().split()))
+ls = [0] + list(map(int,input().split()))
 
-res = sum([data[i] for i in range(K)])
-sum_K = sum([data[i] for i in range(K)])
+prefix_sum = [0] * (N+1)
+prefix_k_sum = [0] * (N+1-K)
 
-for i in range(K, N):
-    sum_K = sum_K - data[i-K] + data[i]
-    res = max([sum_K, res])
+for i in range(1,N+1):
+    prefix_sum[i] = prefix_sum[i-1] + ls[i]
 
-print(res)
+# 1번 방안
+for i in range(K,N+1):
+    prefix_k_sum[i-K] = prefix_sum[i] - prefix_sum[i-K]
+
+print(max(prefix_k_sum))
+
+# 2번 방안
+# result = -100 * 100000
+# for i in range(K,N+1):
+#     result = max(result,prefix_sum[i] - prefix_sum[i-K])
+# print(result)
+
+
