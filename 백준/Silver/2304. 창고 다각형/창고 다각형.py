ced2fc220f4870a0b9fe717749c4f268a3dec192
은석 <122436585+dollseok@users.noreply.github.com>
2024-08-27 20:03:10 +0900

    [Silver II] Title: 창고 다각형, Time: 96 ms, Memory: 110272 KB -BaekjoonHub

--- 백준/Silver/2304. 창고 다각형/창고 다각형.py
+++ 백준/Silver/2304. 창고 다각형/창고 다각형.py
@@ -6,45 +6,43 @@
 1. 가장 높은 것 기준까지 양끝에서 가운데로 모은다
 2. 기준점의 앞뒤로 점화식 형식
 
-
 '''
 
 
-N = int(input())
+n = int(input())
+
 dict = {}
 max_h = 0
-start = 0
-end = 1001
-for _ in range(N):
-    L, H = map(int,input().split())
-    max_h = max(max_h,H)
-    start = min(start,L)
-    end = max(end,L)
-    dict[L] = H
+max_x = 0
+for _ in range(n):
+    x, h = map(int,input().split())
+    max_h = max(h,max_h)
+    max_x = max(x,max_x)
+    dict[x] = h
 
-current = 0
-result = 0
-comeback_idx = 0
+prefix = [0]*(max_x+2)
+middle = 0
 
-for i in range(start,end+1):
-    if i in dict.keys() and current < dict[i]:
-        current = dict[i]
-    result += current
+for i in range(max_x+1):
+    if i in dict.keys():
+        if prefix[i-1] < dict[i]:
+            prefix[i] = dict[i]
+        else:
+            prefix[i] = prefix[i-1]
+    else:
+        prefix[i] = prefix[i-1]
 
-    # 중간 최고값 도착
-    if current == max_h:
-        comeback_idx = i
+    if prefix[i] == max_h:
+        middle = i
         break
 
-# 가장 뒤에서 앞으로
-current = 0
-for j in range(end, comeback_idx,-1):
-    if j in dict.keys() and current < dict[j]:
-        current = dict[j]
-    result += current
+for i in range(max_x,middle,-1):
+    if i in dict.keys():
+        if prefix[i+1] < dict[i]:
+            prefix[i] = dict[i]
+        else:
+            prefix[i] = prefix[i+1]
+    else:
+        prefix[i] = prefix[i+1]
 
-print(result)
-
-
-
-
+print(sum(prefix))
