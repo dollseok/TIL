b66865eff856bee84979d84d39d7b806924fafd6
은석 <122436585+dollseok@users.noreply.github.com>
2026-07-12 20:45:00 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,18 @@
+from collections import deque
+def solution(cards1, cards2, goal):
+    answer = ''
+    q1 = deque(cards1)
+    q2 = deque(cards2)
+    
+    for i in goal:
+        if q1 and i == q1[0]:
+            q1.popleft()
+            goal
+        elif q2 and i == q2[0]:
+            q2.popleft()
+        else:
+            return 'No'
+    return 'Yes'
+    
+    
+    
